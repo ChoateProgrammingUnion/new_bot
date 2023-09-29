e3e91b81315d80a03513167b3f47c50b9ec44f3e
cyrusnaficy <cyrus.naficy@gmail.com>
2023-09-29 15:42:09 -0400

    added db

--- database/db.js
+++ database/db.js
@@ -2,12 +2,14 @@ const colors = require('colors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const config = require('../storage/config.json');
+
 // Check schema folder and user.js
 async function openDatabase() {
 
     mongoose.set('strictQuery', false);
 
-    const connection = await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    const connection = await mongoose.connect(config.dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
     if (connection) {
         console.log(`Database connected`.green);
